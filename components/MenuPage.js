a9f14f3c5469ba7b40bb07a5bb3ba2825d90e80a
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-17 11:56:03 +0100

    added stylesheet

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,15 +1,25 @@
 export class MenuPage extends HTMLElement {
     constructor(){
         super();
-
+        this.root = this.attachShadow({mode: 'open'})
       
+        const styles = document.createElement("style")
+        this.root.appendChild(styles)
+
+        async function loadCSS(){
+            const req = await fetch("/components/MenuPage.css")
+            const css = await req.text()
+            styles.textContent = css
+        }
+
+        loadCSS()
     }
     //when the component is attached to the dom 
     connectedCallback(){
         const template = document.getElementById('menu-page-template')
         //get template val to clone as template val cannot be visible
         const content = template.content.cloneNode(true)
-        this.appendChild(content)
+        this.root.appendChild(content)
     }
 }
 
