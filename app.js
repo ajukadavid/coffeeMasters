e0733c2412b503332376fffd1ec1fef011cd646e
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-17 14:36:28 +0100

    update cart value when item is added

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ window.addEventListener("DOMContentLoaded", () => {
     app.router.init()
 })
 
+window.addEventListener("appcartchange", e => {
+    const badge = document.getElementById("badge")
+    const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0)
+    badge.textContent = qty
+    badge.hidden = qty == 0
+})
