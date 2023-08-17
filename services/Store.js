0b661d8c3461cc55529d9a1ba3f25853e5ee1821
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-17 12:16:06 +0100

    dispatching events when change occurs in store

--- services/Store.js
+++ services/Store.js
@@ -6,7 +6,13 @@
 const proxiedStore = new Proxy(Store, {
     set(target, property, value){
         target[property] = value
-        
+        if(property == "menu"){
+            window.dispatchEvent(new Event("appmenuchange"))
+        }
+        if(property == 'cart'){
+            window.dispatchEvent(new Event("appcartchange"))
+        }
+        return true
     }
 })
 
